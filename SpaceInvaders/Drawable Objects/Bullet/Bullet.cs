963d5d6965f35005f226f990157540468de45f78
Sagi Hirshfeld <sagi.h313@gmail.com>
2018-11-14 21:34:31 +0200

    Sagi: pls

--- SpaceInvaders/Drawable Objects/Bullet/Bullet.cs
+++ SpaceInvaders/Drawable Objects/Bullet/Bullet.cs
@@ -7,7 +7,6 @@ namespace SpaceInvaders
         private const int k_BulletsVelocity = 155;
         public eShootingDirection Direction { get; set; }
 
-
         public Bullet(Game game, string i_SourceFileURL) : base(game, i_SourceFileURL)
         {
             this.Velocity = k_BulletsVelocity;
